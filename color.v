a59390b2d25b1820e7778df3b3e5905731aa3b1d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-29 09:21:54 +0900

    feat: get_color

--- color.v
+++ color.v
@@ -1,5 +1,6 @@
 module cc
 import gg
+// import log
 
 pub fn background(c gg.Color) {
 	mut ctx := context()
@@ -14,5 +15,15 @@ pub fn set_color(c gg.Color){
 	mut ctx := context()
 	if unsafe { ctx.cc != nil } {
 		ctx.cc.current_style.color = c
+	}
+}
+
+pub fn get_color() gg.Color {
+	mut ctx := context()
+	if unsafe { ctx.cc != nil } {
+		return ctx.cc.current_style.color
+	}else{
+		// log.error("cc: failed to get color")
+		panic("cc: failed to get color")
 	}
 }
