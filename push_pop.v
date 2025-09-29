fb372c45e068cf879b8b1d1f05aad0598c72bf1c
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-29 12:56:48 +0900

    MPL-2.0 header

--- push_pop.v
+++ push_pop.v
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 // Reference:
 // https://github.com/openframeworks/openFrameworks/blob/996f7880bc250254480561a2f2dab3554a830084/libs/openFrameworks/gl/ofGLProgrammableRenderer.cpp
 
